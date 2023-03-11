cfe9bb3fbd7107a6c27d1df0fcd08b6c5c524829
train0405 <879386150@qq.com>
2023-03-11 23:55:40 +0800

    update two

--- src/main/java/com/train/springboot_init_maven2/SpringbootInitMaven2Application.java
+++ src/main/java/com/train/springboot_init_maven2/SpringbootInitMaven2Application.java
@@ -3,6 +3,7 @@ package com.train.springboot_init_maven2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.codec.cbor.Jackson2CborDecoder;
 
 @SpringBootApplication
 public class SpringbootInitMaven2Application {
@@ -10,6 +11,7 @@ public class SpringbootInitMaven2Application {
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(SpringbootInitMaven2Application.class, args);
         Object redisTemplate = run.getBean("redisTemplate");
+        Jackson2CborDecoder bean = run.getBean(Jackson2CborDecoder.class);
         System.out.println(redisTemplate);
     }
 
